14b1c147072dc6ddc4e1255b25eb71fa60410678
Kutub Udd <socialnetwork98710@gmail.com>
2024-10-28 23:21:16 +0600

    setup navbar and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,36 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        // Define a custom gradient background using CSS linear-gradient
+        "custom-gradient": "linear-gradient(to right, #050C9C, #3572EF)" // blue-800 to indigo-900 in hex
       },
-    },
+      textColor: {
+        "gradient-transparent": "transparent"
+      },
+      animation: {
+        text: "text 5s ease infinite"
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center"
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center"
+          }
+        }
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+});
